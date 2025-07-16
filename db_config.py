2283d4d253ab340659b3d3de8c6df680c2bd34c7
lucy1234dev <emeraprecious8@gmail.com>
2025-07-16 01:56:23 -0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="dpg-d1qhq52dbo4c73chohd0-",
+        host="dpg-d1qhq52dbo4c73chohd0-a.oregon-postgres.render.com",
         port="5432",
         user="agric_scout_user",
         password="LM2YYoWqUkEAXAMlCVzWLub8fBVEWXbd",
